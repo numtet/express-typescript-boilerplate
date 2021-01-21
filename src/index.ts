67aa9498ef5c51eb5613851a3771e296594d3ccd
numtet <tet.namura@gmail.com>
2021-01-21 17:19:28 +0900

    remove body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import * as bodyParser from "body-parser";
 
 const app = express();
 const router = express.Router();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 router.get(
   "/",
